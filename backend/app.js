2fbcaf89bad458536efcfa3d1305129582099570
Kevin Lavier <kevinlavier@orange.fr>
2024-08-29 13:15:45 +0200

    add : set up database Mongoose

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,28 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const password = require("./password");
+
+mongoose
+  .connect(
+    `mongodb+srv://kevin:${password}@cluster0.tqvp9.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
+  )
+  .then(() => console.log("Connexion à MongoDB réussie !"))
+  .catch(() => console.log("Connexion à MongoDB échouée !"));
 
 const app = express();
+app.use(express.json());
 
-app.use((req, res) => {
-  res.json({ message: "Votre requête a bien été reçue !" });
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  next();
 });
 
 module.exports = app;
