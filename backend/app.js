3609c69173533b9bcbe36585e593a377ab407d74
Kevin Lavier <kevinlavier@orange.fr>
2024-08-29 15:18:38 +0200

    add : routes +/ controllers + middleware to signup and login / install jsonwebtoken and bcrypt

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const userRoutes = require("./routes/user");
 const password = require("./password");
 
 mongoose
@@ -24,5 +25,7 @@ app.use((req, res, next) => {
   );
   next();
 });
+
+app.use("/api/auth", userRoutes);
 
 module.exports = app;
